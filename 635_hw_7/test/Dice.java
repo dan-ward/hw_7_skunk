130026a93bd16360735b2d9255826ea9cea445fc
Eric Helander <hela6565@stthomas.edu>
2017-10-28 11:52:36 -0500

    During class 2017-10-28_1152

---
+++ 635_hw_7/test/Dice.java
@@ -0,0 +1,30 @@
+
+public class Dice {
+
+	private int[] die1Rolls;
+	private int[] die2Rolls;
+	private int loadedDieIndex;
+	
+	public Dice () {
+	}
+	
+	public Dice (int[] die1Rolls, int[] die2Rolls) {
+	}
+	
+	public void roll() {
+	}
+	
+	public int getDie1Roll() {
+		return 0;
+	}
+
+	public int getDie2Roll() {
+		return 0;
+	}	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- 635_hw_7/test/SkunkAppTest.java
+++ 635_hw_7/test/SkunkAppTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SkunkAppTest {
@@ -9,9 +10,23 @@ public class SkunkAppTest {
 	public void setUp() throws Exception {
 	}
 
-	@Test
+	@Ignore
 	public void test() {
 		fail("My first test failure");
 	}
-
+	
+	@Test
+	public void new_dice_have_can_be_created() {
+		Dice d = new Dice();
+		assertTrue("new Dice instance is not null", d!=null);
+	}
+	
+	@Test
+	public void new_dice_will_roll_double_skunk_first() {
+		Dice d = new Dice(new int[] {1}, new int[] {1});
+		d.roll();
+		assertEquals("first die is not 1", 1, d.getDie1Roll());
+		assertEquals("second die is not 1", 1, d.getDie2Roll());
+	}
+	
 }
