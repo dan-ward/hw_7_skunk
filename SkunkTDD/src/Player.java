857e3a4c1a13478533ea805ed2d0ebdb20de1cc8
Eric Helander <hela6565@stthomas.edu>
2017-10-28 10:35:39 -0500

    Skunk code from 2017-10-21

---
+++ SkunkTDD/src/Player.java
@@ -0,0 +1,24 @@
+
+public class Player {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	private String name;
+	private int score;
+	
+	public Player(String name) {
+		this.name = name;
+		this.score = 0;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
+
+}
